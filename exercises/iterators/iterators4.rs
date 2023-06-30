34360a176f365a58b9f307ad669e6efc8624305b
O Hung Lun <hunglun.o@gmail.com>
2023-06-30 16:12:00 +0800

    80% done

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,9 +3,12 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    let mut result = 1;
+    for i in 2..num+1 {
+        result *= i ;
+    }
+    result
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
